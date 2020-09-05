bb9b38433f8a8bde8c83b9f235bdac940f3b8927
salmon <sparksalmon@qq.com>
2020-09-05 10:58:10 +0800

    clean code

--- src/com/leetcode/data/RandomListNode.java
+++
@@ -1,8 +0,0 @@
-package com.leetcode.data;
-
-public class RandomListNode {
-    public int label;
-    public RandomListNode next, random;
-
-    public RandomListNode(int x) { this.label = x; }
-}
