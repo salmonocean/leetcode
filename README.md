d2ed84a0fd80e7c1cec62cfc63b8faf5192e189c
salmon <sparksalmon@qq.com>
2020-09-05 10:58:57 +0800

    Issue: 71

---
+++ README.md
@@ -0,0 +1,9 @@
+LeetCode
+======
+
+### LeetCode Algorithm
+
+| # | Title | Solution | Difficulty | Tag | Familiarity | 
+|---| ----- | -------- | ---------- | --- | ----------- |
+|71| [Simplify Path](https://leetcode-cn.com/problems/simplify-path) | [Java](./src/algorithm/simplifyPath) | Medium | Stack 状态机 | * | 
+
---
+++ src/algorithm/simplifyPath/SimplifyPathTest.java
@@ -0,0 +1,41 @@
+package algorithm.simplifyPath;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.Assert.*;
+
+import java.util.stream.Stream;
+
+public class SimplifyPathTest {
+
+    @ParameterizedTest
+    @MethodSource("testCaseProvider")
+    public void testSimplifyPath(String input, String result) {
+        assertEquals(new SimplifyPath().simplifyPath(input), result);
+    }
+
+    static Stream<Arguments> testCaseProvider() {
+        return Stream.of(
+            Arguments.of("/home/", "/home"), 
+            Arguments.of("/../", "/"),
+            Arguments.of("/home//foo/", "/home/foo"), 
+            Arguments.of("/a/./b/../../c/", "/c"),
+            Arguments.of("/a/../../b/../c//.//", "/c"), 
+            Arguments.of("/a//b////c/d//././/..", "/a/b/c"),
+
+            Arguments.of("/a./", "/a."),
+            Arguments.of("/.a/", "/.a"),
+            Arguments.of("/a.c/", "/a.c"),
+
+            Arguments.of("/a../", "/a.."),
+            Arguments.of("/..a/", "/..a"),
+            Arguments.of("/a..c/", "/a..c"),
+
+            Arguments.of("/a.../", "/a..."),
+            Arguments.of("/...a/", "/...a"),
+            Arguments.of("/a...c/", "/a...c")
+        );
+    }
+}
