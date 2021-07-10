ca79ab451b67f7604c00c0fdea4a8205a5312579
salmon <sparksalmon@qq.com>
2021-07-10 13:07:57 +0800

    style: format

--- src/algorithm/TreeNode.java
+++ src/algorithm/TreeNode.java
@@ -1,11 +1,11 @@
 package algorithm;
 
 public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
+  public int val;
+  public TreeNode left;
+  public TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
-    }
+  TreeNode(int x) {
+    val = x;
+  }
 }
--- src/algorithm/lowestCommonAncestorOfBinarySearchTree/LowestCommonAncestor.java
+++ src/algorithm/lowestCommonAncestorOfBinarySearchTree/LowestCommonAncestor.java
@@ -4,27 +4,27 @@ import algorithm.TreeNode;
 
 public class LowestCommonAncestor {
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        int min, max;
-        if (p.val > q.val) {
-            max = p.val;
-            min = q.val;
-        } else {
-            max = q.val;
-            min = p.val;
-        }
-
-        TreeNode candidate = root;
-        while (candidate != null) {
-            if (candidate.val > max) {
-                candidate = candidate.left;
-            } else if (candidate.val < min) {
-                candidate = candidate.right;
-            } else {
-                return candidate;
-            }
-        }
-
-        return null;
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    int min, max;
+    if (p.val > q.val) {
+      max = p.val;
+      min = q.val;
+    } else {
+      max = q.val;
+      min = p.val;
     }
+
+    TreeNode candidate = root;
+    while (candidate != null) {
+      if (candidate.val > max) {
+        candidate = candidate.left;
+      } else if (candidate.val < min) {
+        candidate = candidate.right;
+      } else {
+        return candidate;
+      }
+    }
+
+    return null;
+  }
 }
